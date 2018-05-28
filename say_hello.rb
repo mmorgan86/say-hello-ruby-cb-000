016a4ffd6188c5d36e6d1ef1e42bd4abfc0f21d1
Michael Morgan <michael1986morgan@gmail.com>
2018-05-28 04:30:05 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
 # I call on the method, say_hello, and give it the string "Gabriela"
 def say_hello(name = "Ruby Programmer!")
-  puts name
+  puts "Hello #{name}"
 end
